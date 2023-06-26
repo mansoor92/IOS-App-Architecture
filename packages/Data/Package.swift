624a5694d46a9127fc36c04ca4039d9c9cafff38
Mansoor Ali <mali92390@gmail.com>
2023-06-26 16:23:23 +0500

    added missing package file

---
+++ packages/Data/Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Data",
+    platforms: [.iOS("13.0")],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "Data",
+            targets: ["Data"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/mansoor92/ApiClient", branch: "async-await")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "Data",
+            dependencies: [.product(name: "ApiClient", package: "ApiClient")] ),
+        .testTarget(
+            name: "DataTests",
+            dependencies: ["Data"]),
+    ]
+)
